22bfbfc7fa56b39403e04ea1b3cbfc11dc3d4886
mrz <videojuego740@gmail.com>
2026-07-25 20:37:42 +0200

    feat: create SurfaceMovementComponent
    - Create SurfacePhaseParticipant interface class
    - Fix empty whitespace in copyright notice

---
+++ Plugins/SurfaceNavigation/Source/SurfaceNavigation/Public/Movement/SurfaceMovementComponent.h
@@ -0,0 +1,54 @@
+// Copyright (c) Mrz740. Licensed under the MIT License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Core/SurfacePhaseParticipant.h"
+#include "SurfaceMovementComponent.generated.h"
+
+USTRUCT()
+struct FSurfaceProbeResult
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	bool bIsOnSurface = false;
+	UPROPERTY()
+	FVector SurfaceNormal = FVector::UpVector;
+	UPROPERTY()
+	FVector ImpactPoint = FVector::ZeroVector;
+};
+
+USTRUCT()
+struct FSurfaceState
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	bool bIsOnSurface = false;
+	UPROPERTY()
+	FVector SurfaceNormal = FVector::UpVector;
+	UPROPERTY()
+	FVector ImpactPoint = FVector::ZeroVector;
+};
+
+UCLASS(ClassGroup=(Movement), meta=(BlueprintSpawnableComponent))
+class SURFACENAVIGATION_API USurfaceMovementComponent : public UActorComponent, public ISurfacePhaseParticipant
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	FSurfaceProbeResult PendingProbeResult;
+	UPROPERTY()
+	FSurfaceState CommittedState;
+	UPROPERTY(EditAnywhere)
+	float ProbeDistance = 100.0f;
+	
+public:
+	USurfaceMovementComponent();
+
+	virtual void ExecuteReadPhase() override;
+	virtual void ExecuteSimulatePhase() override;
+	virtual void ExecuteCommitPhase() override;
+};
